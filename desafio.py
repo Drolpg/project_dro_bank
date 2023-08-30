dc9213a09b2d575edc5ee7e9ebaf2f2e43ea866b
Drolpg <leandronascimento.drolpg21@gmail.com>
2023-08-30 16:18:53 -0300

    Correção v1

--- desafio.py
+++ desafio.py
@@ -22,36 +22,36 @@ digite a opção desejada:
 while True:
     opcao = input(menu)
     if opcao == "1":
-        print(f"Saldo: {saldo:.2f}")
+        print(f"\nSaldo: R${saldo:.2f}")
 
     elif opcao == "2":
-        if saque_efetuado <= LIMITE_SAQUES:
+        if saque_efetuado < LIMITE_SAQUES:
             print(
-                f"Você ja efetuo {saque_efetuado} Hoje... O Limite é de 3 saques diarios")
+                f"\nVocê ja efetuo {saque_efetuado} saques Hoje...\nO Limite é de 3 saques diarios")
             valor_saque = int(input("Digite o valor desejado: "))
             if valor_saque > saldo:
                 print(
-                    f"Operação não realizada saldo insuficiente... Saldo atual: {saldo:.2f}")
+                    f"\nOperação não realizada saldo insuficiente...\n\nSaldo atual: R${saldo:.2f}")
             else:
                 saldo = saldo - valor_saque
                 saque_efetuado = saque_efetuado + 1
                 print(
-                    f"Operação realizada com SUCESSO... Saque efetuado no valor de {valor_saque:.2f} Saldo atial: {saldo:.2f}")
+                    f"Operação realizada com SUCESSO...\n\nSaque efetuado no valor de {valor_saque:.2f}\n\nSaldo atual: R${saldo:.2f}")
                 quat_saque = quat_saque + 1
         else:
-            print("Limite de saque atingido... tente novamente amanha.")
+            print("\nLimite de saque atingido... tente novamente amanha.\n")
 
     elif opcao == "3":
-        print("Deposito")
+        print("\nDeposito\n")
         valor_deposito = int(input("Digite o valor que deseja depositar: "))
         saldo = saldo + valor_deposito
         print(
-            f"Operação realizada com SUCESSO... Deposito efetuado no valor de {valor_deposito:.2f} Saldo atial: {saldo:.2f}")
+            f"\nOperação realizada com SUCESSO...\n\nDeposito efetuado no valor de {valor_deposito:.2f}\n\nSaldo atual: R${saldo:.2f}")
         quant_deposito = quant_deposito = + 1
 
     elif opcao == "4":
         print(
-            f"Extrato... - Quantidade de saque: {quat_saque} - Quantidade de depositos: {quant_deposito} - Saldo atual: {saldo:.2f}")
+            f"Extrato...\n\n- Quantidade de saque: {quat_saque}\n\n- Quantidade de depositos: {quant_deposito}\n\n- Saldo atual: R${saldo:.2f}")
 
     elif opcao == "0":
         print("Finalizando o Sistema...")
